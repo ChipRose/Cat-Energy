3f27997ea1ab1df211f386ae0fc78523a1bb456d
Yana Rozova <yana.rozova@mail.ru>
2021-10-21 10:37:45 +0300

    js for slider work

---
+++ source/js/slider.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector(".slider");
+const buttonBefore = document.querySelector(".slider__button--before");
+const buttonAfter = document.querySelector(".slider__button--after");
+let count = 0;
+
+buttonBefore.addEventListener("click", function () {
+  if (slider.classList.contains("slider--init")) {
+    slider.classList.remove("slider--init");
+    slider.classList.add("slider--before");
+  } else {
+    if (slider.classList.contains("slider--before")) {
+      slider.classList.remove("slider--before");
+      slider.classList.add("slider--init");
+    }
+  }
+  if (slider.classList.contains("slider--after")) {
+    slider.classList.remove("slider--after");
+    slider.classList.add("slider--before");
+  }
+});
+
+buttonAfter.addEventListener("click", function () {
+  if (slider.classList.contains("slider--init")) {
+    slider.classList.remove("slider--init");
+    slider.classList.add("slider--after");
+  } else {
+    if (slider.classList.contains("slider--after")) {
+      slider.classList.remove("slider--after");
+      slider.classList.add("slider--init");
+    }
+  }
+  if (slider.classList.contains("slider--before")) {
+    slider.classList.remove("slider--before");
+    slider.classList.add("slider--after");
+  }
+});
