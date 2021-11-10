9aaf55076b35c79a760f97d459b80c21d8c17f73
Yana Rozova <yana.rozova@mail.ru>
2021-11-10 17:43:13 +0300

    Automatization and build added

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,23 @@
+//Burger
+
+const navigation = document.querySelector(".navigation");
+const burger = document.querySelector(".navigation__toggle");
+const navigationItems = document.querySelectorAll (".navigation__item");
+
+navigation.classList.remove("navigation--nojs");
+
+burger.addEventListener ("click", function(){
+  if (navigation.classList.contains("navigation--closed")) {
+    navigation.classList.remove("navigation--closed");
+    navigation.classList.add("navigation--opened");
+  } else {
+    navigation.classList.add("navigation--closed");
+    navigation.classList.remove("navigation--opened");
+  }
+});
+
+//Slider
+
 const slider = document.querySelector(".slider");
 const buttonBefore = document.querySelector(".slider__button--before");
 const buttonAfter = document.querySelector(".slider__button--after");
