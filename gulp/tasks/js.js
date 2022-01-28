5bff8c1af0cd3d60fd778458729d9ca337c2532e
Yana Rozova <yana.rozova@mail.ru>
2022-01-28 12:39:36 +0300

    update package.json

---
+++ gulp/tasks/js.js
@@ -0,0 +1,17 @@
+const terser = require("gulp-terser");
+
+export const js = () => {
+  return app.gulp.src(app.path.src.js, { sourcemaps: true })
+    .pipe(app.plugins.plumber(
+      app.plugins.notify.onError({
+        title: "JS",
+        message: "Error: <%= error.message %>"
+      })
+    ))
+    .pipe(terser())
+    .pipe(app.plugins.rename({
+      extname:".min.js"
+    }))
+    .pipe(app.gulp.dest(app.path.build.js))
+    .pipe(app.plugins.browsersync.stream());
+}
