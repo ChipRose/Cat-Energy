e17363fd03d8aba9578a755b3790b74c32d8dab0
Yana Rozova <yana.rozova@mail.ru>
2022-01-28 14:52:19 +0300

    path to image fixed

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -13,7 +13,6 @@ export const scss = () => {
         message: "Error: <%= error.message %>"
       })
     ))
-    .pipe(app.plugins.replace(/@img\//g, "../img/"))
     .pipe(gulpSass())
     .pipe(groupCssMediaQueries())
     .pipe(postcss([
