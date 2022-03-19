be6f0ac209febc0f576f035ac28660361aec7d9c
Yana Rozova <yana.rozova@mail.ru>
2022-03-20 00:59:22 +0300

    fix problem with map pin size

--- source/js/map.js
+++ source/js/map.js
@@ -1,70 +1,78 @@
+const WINDOW_LIMIT = 768;
+
 const Coordinates = {
   LAT: 59.938635,
   LNG: 30.323118,
   ZOOM: 20,
 };
 
-let PinSize = {
-  PIN: {
-    X: 47,
-    Y: 53,
-  },
-  SHADOW: {
-    X: 47,
-    Y: 8,
-  },
-};
+const mapCanvas = document.querySelector('.address__map-block');
 
-const WindowWidth = {
-  TABLE: 768,
-};
-
-const windowInnerWidth = window.innerWidth;
-if (windowInnerWidth > WindowWidth.TABLE) {
-  PinSize = {
-    PIN: {
-      X: 113,
-      Y: 106,
-    },
-    SHADOW: {
-      X: 90,
-      Y: 18,
-    },
-  };
+const changePinSize = () => {
+  return window.innerWidth >= WINDOW_LIMIT ? true : false;
 }
 
+window.addEventListener('resize', () => {
+  let PinSize = {
+    PIN: {
+      X: 47,
+      Y: 53,
+    },
+    SHADOW: {
+      X: 47,
+      Y: 8,
+    },
+  };
 
-const mapCanvas = document.querySelector('.address__map-block');
-mapCanvas.innerHTML = '';
-mapCanvas.innerHTML = '<div id="map-inner" style="width: 100%; height: 100%;"></div>';
-
-const map = L.map('map-inner', {
-  scrollWheelZoom: false,
-})
-  .setView([Coordinates.LAT, Coordinates.LNG], Coordinates.ZOOM);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-})
-  .addTo(map);
-
-const mapIcon = L.icon({
-  iconUrl: '../img/icons/map-pin.svg',
-  shadowUrl: '../img/background/pin-shadow-tablet.png',
-
-  iconSize: [PinSize.PIN.X, PinSize.PIN.Y],
-  shadowSize: [PinSize.SHADOW.X, PinSize.SHADOW.Y],
-  iconAnchor: [PinSize.PIN.X / 2, PinSize.PIN.Y],
-  shadowAnchor: [PinSize.SHADOW.X / 2, PinSize.SHADOW.Y],
+  if (changePinSize()) {
+    PinSize = {
+      PIN: {
+        X: 113,
+        Y: 106,
+      },
+      SHADOW: {
+        X: 90,
+        Y: 18,
+      },
+    };
+  }
+  setMap(PinSize);
 });
 
-const marker = L.marker({
-  lat: Coordinates.LAT,
-  lng: Coordinates.LNG,
-},
-  {
-    draggable: false,
-    icon: mapIcon,
-  },
-);
 
-marker.addTo(map);
+const setMap = (iconsSizes) => {
+  let {PIN, SHADOW} = iconsSizes;
+  mapCanvas.innerHTML = '';
+  mapCanvas.innerHTML = '<div id="map-inner" style="width: 100%; height: 100%;"></div>';
+
+  const map = L.map('map-inner', {
+    scrollWheelZoom: false,
+  })
+    .setView([Coordinates.LAT, Coordinates.LNG], Coordinates.ZOOM);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+    .addTo(map);
+
+  const mapIcon = L.icon({
+    iconUrl: '../img/icons/map-pin.svg',
+    shadowUrl: '../img/background/pin-shadow-tablet.png',
+
+    iconSize: [PIN.X, PIN.Y],
+    shadowSize: [SHADOW.X, SHADOW.Y],
+    iconAnchor: [PIN.X / 2, PIN.Y],
+    shadowAnchor: [SHADOW.X / 2, SHADOW.Y],
+  });
+
+  const marker = L.marker({
+    lat: Coordinates.LAT,
+    lng: Coordinates.LNG,
+  },
+    {
+      draggable: false,
+      icon: mapIcon,
+    },
+  );
+
+  marker.addTo(map);
+};
